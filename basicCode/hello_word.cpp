c607db951f977e82b5ed1995d8bdbf14b0926ec5
HEYU YU <heyuyu@spartan-login3.hpc.unimelb.edu.au>
2025-12-03 23:00:48 +1100

    hello_word added

---
+++ basicCode/hello_word.cpp
@@ -0,0 +1,18 @@
+/*
+ * File: hello.c
+ * Description: A simple C program that prints "Hello World!" to the screen.
+ * Author: your_name
+ * Date: yyyy-mm-dd
+ */
+
+#include <stdio.h>   // 包含标准输入输出库
+
+// 主函数：程序从这里开始执行
+int main(void) {
+    // 打印字符串到屏幕
+    printf("Hello World!\n");
+
+    // 返回 0 表示程序成功结束
+    return 0;
+}
+
